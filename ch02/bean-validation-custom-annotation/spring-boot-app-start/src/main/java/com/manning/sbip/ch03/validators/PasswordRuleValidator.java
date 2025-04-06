948e248388660d19cd7411e9635b51e131c22367
DavJaveiro <davidson.linhares@outlook.com>
2025-04-06 15:58:43 -0300

    todays study

---
+++ ch02/bean-validation-custom-annotation/spring-boot-app-start/src/main/java/com/manning/sbip/ch03/validators/PasswordRuleValidator.java
@@ -0,0 +1,7 @@
+package com.manning.sbip.ch03.validators;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordRuleValidator implements ConstraintValidator<Password, String> {
+}
