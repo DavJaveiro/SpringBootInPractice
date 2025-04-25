961a389f31bc1434fd7dc5963280476ac91e926c
Davidson de Souza Linhares <davidson.linhares@outlook.com>
2025-04-24 22:19:45 -0300

    todays

--- ch03/using-query-annotation/course-tracker-start/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
+++ ch03/using-query-annotation/course-tracker-start/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -1,8 +1,35 @@
 package com.manning.sbip.ch03;
 
+import com.manning.sbip.ch03.repository.CourseRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CourseTrackerSpringBootApplicationTests {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void givenCoursesCreatedWhenLoadCoursesWithQueryThenExpectCorrectCourseDetails(){
+        assertThat(courseRepository.findAllByCategory("Spring")).hasSize(8);
+        courseRepository.findAllByCategory("Spring")
+                .forEach(course -> System.out.println(course));
+
+        assertThat(courseRepository.findAllByCategoryAndRatingGreaterThan("Spring", 4))
+                .hasSize(3);
+        courseRepository.findAllByCategoryAndRatingGreaterThan("Spring", 4).forEach(course -> System.out.println(course));
+
+        assertThat(courseRepository.findAllByRating(5)).hasSize(3);
+        courseRepository.findAllByRating(4).forEach(course -> System.out.println(course));
+
+        assertThat(courseRepository.updateRating(1, "Spring Boot with React")).isEqualTo(1);
+    }
+
+
+
 
 }
