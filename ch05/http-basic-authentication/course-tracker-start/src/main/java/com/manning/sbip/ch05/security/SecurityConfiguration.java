ecc02e4cec2c6d0677c83d88791cd625b84d4fdf
Davidson de Souza Linhares <davidson.linhares@outlook.com>
2025-06-11 21:52:42 -0300

    todays study

--- ch05/http-basic-authentication/course-tracker-start/src/main/java/com/manning/sbip/ch05/security/SecurityConfiguration.java
+++ ch05/http-basic-authentication/course-tracker-start/src/main/java/com/manning/sbip/ch05/security/SecurityConfiguration.java
@@ -20,6 +20,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and().httpBasic();
 
     }
 
